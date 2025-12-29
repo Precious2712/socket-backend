bb2ff549b08fc1817e07635456de593e05cad99c
Precious2712 <oluwanifemimoonlight@gmail.com>
2025-12-28 23:34:42 -0800

    included one changed

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,11 @@ async function bootstrap() {
       'http://localhost:3001',
       'http://localhost:3002',
     ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
     credentials: true,
   });
+
 
   app.useGlobalPipes(new ValidationPipe());
 
